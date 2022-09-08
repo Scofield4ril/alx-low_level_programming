6b0ee068f090b7b600106521a7bd93c7490260ef
scofield4ril <oluwasegunfunmi2015@gmail.com>
2022-09-08 15:29:04 -0700

    Task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 * main - print the string in the put function
 *
 * Description: using the main funtion
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "Programming is like building a multilingual puzzle"
 * Return: 0
 */
 int main(void)
