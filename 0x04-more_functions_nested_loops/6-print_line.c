1fb667c552d2b3666d011fcada2dc31fa0f1c195
scofield4ril <oluwasegunfunmi2015@gmail.com>
2022-09-23 13:46:14 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * prints_line - draws a straight line in the terminal
+ * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
 void print_line(int n)
@@ -17,7 +17,7 @@ void print_line(int n)
 		{
 			_putchar('_');
 		}
-		_putchar
+		_putchar('\n');
 	}
 
 }
