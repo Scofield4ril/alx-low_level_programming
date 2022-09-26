df46987b2b59dc449d8f1c120c37c92bbecd662c
scofield4ril <oluwasegunfunmi2015@gmail.com>
2022-09-26 15:11:27 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies memory are
+ * *_memcpy - copies memory area
  * @dest: destination memory area
  * @src: memory area to copy from
  * @n: number of bytes to copy
@@ -10,7 +10,7 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i:
+	unsigned int i;
 
 		for (i = 0; i < n; i++)
 		{
