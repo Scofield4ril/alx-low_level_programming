46f2e5fcef6c6a2a06314e9982e032d976bce222
scofield4ril <oluwasegunfunmi2015@gmail.com>
2022-10-05 21:38:19 -0700

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,30 +11,30 @@
 int main(int argc, char *argv[])
 {
 	int a, n = 0, i, t;
-	int c[s] = {25, 10, 5, 2, 1};
+	int c[5] = {25, 10, 5, 2, 1};
 
 		if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
 		{
-			puts("Error");
-			return (1);
+			t = a / c[i];
+			a -= t * c[i];
+			n += t;
+			if (a == 0)
+				break;
 		}
-		a = atoi(argv[1]);
-		if (a <= 0)
-		{
-			puts("0");
-			return (1);
-		}
-		else
-		{
-			for (i = 0; i < 5; i++)
-			{
-				t = a / c[i];
-				a -= t * c[i];
-				n += t;
-				if (a == 0)
-					break;
-			}
-		}
-		printf("%d\n", n);
-		return (0);
+	}
+	printf("%d\n", n);
+	return (0);
 }
